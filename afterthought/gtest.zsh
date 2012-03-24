00d30a72d13b6e0dc751289d54d9456e72e155e1
Alex Duller <alexduller@gmail.com>
2012-03-24 23:46:07 +0000

    Added afterthought stuff

--- afterthought/gtest.zsh
+++ afterthought/gtest.zsh
@@ -1,3 +1,7 @@
-export GTEST_INCLUDE=/Users/alexduller/gtest-1.6.0/include
 export GTEST_LIBRARIES=/Users/alexduller/gtest-1.6.0/build/lib/.libs
-export DYLD_LIBRARY_PATH=${GTEST_LIBRARIES}
+export GMOCK_LIBRARIES=/Users/alexduller/gmock-1.6.0/lib
+
+export GTEST_ROOT=/Users/alexduller/gtest-1.6.0
+export GMOCK_ROOT=/Users/alexduller/gmock-1.6.0
+
+export DYLD_LIBRARY_PATH=${GTEST_LIBRARIES}:${GMOCK_LIBRARIES}:${LIBGIT_LIBRARIES}
---
+++ afterthought/paths.zsh
@@ -0,0 +1,8 @@
+export PATH=$HOME/afterthought-software/klee/Release+Asserts/bin:$PATH
+export PATH=$HOME/afterthought-software/stp-install/bin:$PATH
+export PATH=$HOME/afterthought-software/z3/bin:$PATH
+
+export PATH=$HOME/afterthought-software/afterthought/scripts:$PATH
+
+export PATH=$HOME/afterthought-software/modguard/scripts:$PATH
+export PATH=$HOME/afterthought-software/modguard/build/src/verify:$PATH
